529d1585066712f58fd319c4747fe5035a8dfd3b
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-05-31 22:19:32 -0400

    Teach the knob on the iris to do volume
    and remove some unncecessary code in keymap.c

--- keebio/iris/keymaps/solvaholic/config.h
+++ keebio/iris/keymaps/solvaholic/config.h
@@ -19,3 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
+// #define ENCODERS_PAD_A { B12 }
+// #define ENCODERS_PAD_B { B13 }
+// #define ENCODER_DIRECTION_FLIP
+#define ENCODER_RESOLUTION 4
